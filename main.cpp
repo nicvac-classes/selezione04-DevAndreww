bc4b6c4eeb7842107ea1ccf3414803c9819038d8
DevAndreww <andrea.sette2@marconibari.edu.it>
2024-09-26 16:20:50 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main() 
+{
+    int num1, num2, num3, max;
+
+    cout << "Inserisci 3 numeri e io ti dirò quello maggiore." << endl;
+
+    cin >> num1;
+    cin >> num2;
+    cin >> num3;
+
+    if(num1>num2)
+    {
+        if(num1>num3)
+        {
+            max = num1;
+        }
+        else
+        {
+            max = num3;
+        }
+    }
+    else
+    {
+        if(num2>num3)
+        {
+            max = num2;
+        }
+        else
+        {
+            max = num3;
+        }
+    }
+
+    cout << "Il numero maggiore è " << max << "." << endl;
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
